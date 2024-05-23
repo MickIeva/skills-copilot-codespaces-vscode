9d7762452123abcb79e6d02ec3809ee6d6e7e11d
MickIeva <168742871+MickIeva@users.noreply.github.com>
2024-05-23 10:03:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,33 +1,33 @@
 // create web server
+// create a route for comments
+// create a route for comments/:id
+// create a route for comments/:id/edit
+// create a route for comments/:id/delete
+
 var express = require('express');
 var app = express();
-var bodyParser = require('body-parser');
-var fs = require('fs');
+var port = 3000;
 
-// create application/x-www-form-urlencoded parser
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-// create application/json parser
-var jsonParser = bodyParser.json();
-
-var comments = require('./comments.json');
-
-app.get('/comments', function (req, res) {
-    res.json(comments);
+// create a route for comments
+app.get('/comments', function(req, res) {
+  res.send('This is the comments page');
 });
 
-app.post('/comments', jsonParser, function (req, res) {
-    comments.push(req.body);
-    fs.writeFile('./comments.json', JSON.stringify(comments), function (err) {
-        if (err) {
-            console.log(err);
-            res.status(500).send('Error saving comment');
-        } else {
-            res.status(201).send('Comment saved');
-        }
-    });
+// create a route for comments/:id
+app.get('/comments/:id', function(req, res) {
+  res.send('This is the comment page for comment id: ' + req.params.id);
 });
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+// create a route for comments/:id/edit
+app.get('/comments/:id/edit', function(req, res) {
+  res.send('This is the edit page for comment id: ' + req.params.id);
+});
+
+// create a route for comments/:id/delete
+app.get('/comments/:id/delete', function(req, res) {
+  res.send('This is the delete page for comment id: ' + req.params.id);
+});
+
+app.listen(port, function() {
+  console.log('Server is running on port', port);
 });
